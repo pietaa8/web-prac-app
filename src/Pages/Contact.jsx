efd9d5a99e30504fa2b7b048384b1adaefafc7d3
pietaa8 <tasnianafisa509@gmail.com>
2025-07-30 02:19:00 +0600

    added footer

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -10,6 +10,45 @@ const Contact = () => {
           Got a technical issue? want to send feedback about a beta feature? Let
           us Know.
         </p>
+        <form action="#" className="space-y-8">
+          <div>
+            <label htmlFor="email" className="form__label">
+              Your Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              placeholder="example@gmail.com"
+              className="form__input mt-1"
+            />
+            <div className="mt-5">
+              <label htmlFor="subject" className="form__label">
+                Subject
+              </label>
+              <input
+                type="text"
+                id="subject"
+                placeholder="Let us known how we can help you"
+                className="form__input mt-1"
+              />
+            </div>
+            <div className="sm:col-span-2 mt-5">
+              <label htmlFor="message" className="form__label">
+                Your Message
+              </label>
+              <textarea
+                type="text"
+                id="message"
+                rows="6"
+                placeholder="Leave a commeny"
+                className="form__input mt-1"
+              />
+            </div>
+          </div>
+          <button type="submit" className="btn rounded sm:w-fit">
+            Submit
+          </button>
+        </form>
       </div>
     </section>
   );
