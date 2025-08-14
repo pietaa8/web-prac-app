5301990415412bfa01c629ffe96824cc2354e38e
dipanitaS5 <dipanitasikder58@gmail.com>
2025-08-15 02:10:48 +0600

    added questioners in faqs.js

--- data/faqs.js
+++ data/faqs.js
@@ -1,18 +1,23 @@
 export const faqs = [
   {
+    question: "How do I know the consultant is qualified?",
+    content:
+      "All our legal consultants go through a strict verification process to ensure they are licensed professionals with proven expertise in their field.",
+  },
+  {
+    question: "Is my personal information kept confidential?",
+    content:
+      "Absolutely. We follow strict privacy policies and secure data protection measures to ensure your information remains confidential at all times.",
+  },
+  {
     question: "What kind of legal services do you offer?",
     content:
       "We connect you with experienced legal consultants across various fields like family law, property, business, and more. Our goal is to make legal help accessible, reliable, and affordable using a user-friendly platform and expert support.",
   },
   {
-    question: "What if I need to attend a court hearing?",
+    question: "Do you provide help with legal documentation?",
     content:
-      "Our consultants can advise you on how to prepare for court hearings. While we don't physically represent clients, we ensure you're legally informed and supported before, during, and after the process.",
-  },
-  {
-    question: "What if I need urgent legal advice?",
-    content:
-      "You can instantly book an urgent consultation through our platform. We have legal experts available for time-sensitive cases involving contracts, disputes, or emergency legal situations.",
+      "Yes, our consultants assist with reviewing, drafting, and understanding legal documents such as contracts, agreements, affidavits, and more.",
   },
   {
     question: "Can I meet the legal consultant in person?",
@@ -20,8 +25,13 @@ export const faqs = [
       "Most of our consultations are conducted online for your convenience. However, if required, some consultants may offer in-person meetings depending on your location and case type.",
   },
   {
-    question: "Do you provide help with legal documentation?",
+    question: "What if I need urgent legal advice?",
     content:
-      "Yes, our consultants assist with reviewing, drafting, and understanding legal documents such as contracts, agreements, affidavits, and more.",
+      "You can instantly book an urgent consultation through our platform. We have legal experts available for time-sensitive cases involving contracts, disputes, or emergency legal situations.",
+  },
+  {
+    question: "What if I need to attend a court hearing?",
+    content:
+      "Our consultants can advise you on how to prepare for court hearings. While we don't physically represent clients, we ensure you're legally informed and supported before, during, and after the process.",
   },
 ];
